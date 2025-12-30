3dad51614e584c110c1398662df7077bdc6db4bc
Sagor8187 <sdsagor8187@gmail.com>
2025-12-30 11:32:56 +0600

    addd animation

--- script.js
+++ script.js
@@ -1,83 +1,116 @@
-        let addbtn = document.getElementById("addbtn")
-        let tasklist = document.getElementById("tasklist")
-        let input = document.getElementById("taskinput")
-        let timervalue = document.getElementById("timervalue")
-        let deletelist =document.getElementById("delete")
-        let completeitem = document.getElementById("complete")
+let addbtn = document.getElementById("addbtn")
+let tasklist = document.getElementById("tasklist")
+let input = document.getElementById("taskinput")
+let timervalue = document.getElementById("timervalue")
+let deletelist =document.getElementById("delete")
+let completeitem = document.getElementById("complete")
 
-    
+addbtn.addEventListener("click",() =>{
 
-        addbtn.addEventListener("click",() =>{
-           let text = input.value.trim();
-           if (text === "") return alert("Please add task")
-           let li = document.createElement("li")
-           let now = new Date();
-           let time = now.toLocaleTimeString(); 
-           let taskspan = document.createElement("span")
-           taskspan.innerText=`${text} (added : ${time})`
-           let timerspan = document.createElement("span")
-           timerspan.style.marginLeft ="10px"
-           timerspan.style.color ="red"
-           li.appendChild(taskspan)
-           li.appendChild(timerspan)
-           let dltbtn = document.createElement("button")
-           dltbtn.innerText = "❌"
+   let text = input.value.trim();
+   if (text === "") return alert("Please add task")
 
+   let li = document.createElement("li")
+   li.style.transition=".3s"
 
-           dltbtn.addEventListener("click",()=>{
-            clearInterval(timer)
+   let now = new Date();
+   let time = now.toLocaleTimeString(); 
+
+   let taskspan = document.createElement("span")
+   taskspan.innerText=`${text} (added : ${time})`
+
+   let timerspan = document.createElement("span")
+   timerspan.style.marginLeft ="10px"
+   timerspan.style.color ="yellow"
+
+   li.appendChild(taskspan)
+   li.appendChild(timerspan)
+
+   // delete button
+   let dltbtn = document.createElement("button")
+   dltbtn.innerText = "❌"
+
+   dltbtn.addEventListener("click",()=>{
+
+    clearInterval(timer)
+
+    li.style.transform="translateX(60px)"
+    li.style.opacity="0"
+
+    setTimeout(()=>{
+        if(tasklist.contains(li)){
             tasklist.removeChild(li)
 
             let deleteitem = document.createElement("li")
-            deleteitem.innerText =`${text} -- deleted manual at (${new Date().toLocaleTimeString()})`
+            deleteitem.innerText =`${text} -- deleted manually at (${new Date().toLocaleTimeString()})`
             deletelist.appendChild(deleteitem)
-           })
+        }
+    },200)
 
-           let compbtn = document.createElement("button")
-           compbtn.innerText = "✅"
-           compbtn.style.marginLeft ="10px"
-           compbtn.addEventListener("click",()=>{
-            clearInterval(timer)
+   })
+
+   // complete button
+   let compbtn = document.createElement("button")
+   compbtn.innerText = "✅"
+
+   compbtn.addEventListener("click",()=>{
+
+    clearInterval(timer)
+
+    li.style.boxShadow="0 0 15px lightgreen"
+    li.style.transform="translateY(-3px)"
+
+    setTimeout(()=>{
+        if(tasklist.contains(li)){
+            tasklist.removeChild(li)
+
+            let completelist = document.createElement("li")
+            completelist.innerText =`${text} -- completed at (${new Date().toLocaleTimeString()})`
+            completeitem.appendChild(completelist)
+        }
+    },200)
+
+   })
+
+   li.appendChild(compbtn)
+   li.appendChild(dltbtn)
+   tasklist.appendChild(li)
+
+   input.value =""
+
+   let useminit = parseInt(timervalue.value);
+   if (isNaN(useminit)||useminit <= 0) useminit=1
+   timervalue.value=""
+
+   let timeleft = useminit*60
+
+   let timer =  setInterval(()=>{
+
+    let minit = Math.floor(timeleft/60)
+    let sec = timeleft % 60
+
+    let formate = `${minit.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`
+
+    timerspan.innerText=`⏳ ${formate}`
+
+    if(timeleft <=0){
+        clearInterval(timer)
+
+        li.style.opacity=".3"
+        li.style.transform="translateX(-40px)"
+
+        setTimeout(()=>{
+
             if(tasklist.contains(li)){
-                tasklist.removeChild(li);
+                tasklist.removeChild(li)
 
-                let completelist = document.createElement("li")
-                completelist.innerText =`${text} -- complete (${new Date().toLocaleTimeString()})`
-                completeitem.appendChild(completelist)
+                let deleteitem = document.createElement("li")
+                deleteitem.innerText =`${text} -- auto deleted at (${new Date().toLocaleTimeString()})`
+                deletelist.appendChild(deleteitem)
             }
-           })
+        },200)
+    }
 
-
-           li.appendChild(compbtn)
-           li.appendChild(dltbtn)
-           tasklist.appendChild(li)
-           input.value ="" 
-           
-           let useminit = parseInt(timervalue.value);
-           if (isNaN(useminit)||useminit <= 0) useminit=1
-           timervalue.value=""
-           
-           let timeleft = useminit*60
-           let timer =  setInterval(()=>{
-            let minit = Math.floor(timeleft/60)
-            let seceond = timeleft % 60
-            let formate = `${minit.toString().padStart(2,"0")}:${seceond.toString().padStart(2,"0")}`
-            timerspan.innerText=`(Remaining :${formate})`
-
-            if(timeleft <=0){
-                clearInterval(timer)
-                if(tasklist.contains(li)){
-                    tasklist.removeChild(li)
-                    alert(`${text} is remove `)
-
-                    let deleteitem = document.createElement("li")
-                    deleteitem.innerText =`${text} -- deleted auto at (${new Date().toLocaleTimeString()})`
-                    deletelist.appendChild(deleteitem)
-                    
-                }
-            }
-            timeleft--
-
-
-           },1000)
-        })
+    timeleft--
+   },1000)
+})
